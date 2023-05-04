82d3a58e803c0f5458e8d98635985ee6b917a90a
Noel Emaas <nemaas@gbox.adnu.edu.ph>
2023-05-04 22:40:42 +0800

    feat: added decimal to binary conversion

--- main.asm
+++ main.asm
@@ -1,48 +1,71 @@
-;predefined directives
-SYS_EXIT  equ 1
-SYS_READ  equ 3
-SYS_WRITE equ 4
-STDIN     equ 0
-STDOUT    equ 1
+SYS_EXIT    equ 1
+SYS_READ    equ 3
+SYS_WRITE   equ 4
+STDIN       equ 0
+STDOUT      equ 1
 
-;macros for printing
-%macro print 2 
-    mov eax, SYS_WRITE
-    mov ebx, STDOUT
-    mov ecx, %1
-    mov edx, %2
-    int 80h
-%endmacro
-
-;macros for getting input
-%macro read 2
-    mov eax, SYS_READ
-    mov ebx, STDIN
-    mov ecx, %1
-    mov edx, %2
-    int 80h
-%endmacro
+%include    'functions.asm'
+%include    'atoi.asm'
 
 section .data
-    data_size_prompt db 'Enter the number of data: ', 0xa
-    data_size_prompt_len equ $-data_size_prompt
-    data_input_prompt db 'Enter the numbers: ', 0xa
-    data_input_prompt_len equ $-data_input_prompt
+    ip_prompt       db  "Enter a decimal number to be converted to binary: ", 0h
+    binary_digits   db  32 dup('0')
+    binary_len      dw  0
 
 section .bss
-    data_size resd 2
-    data times 100 resd 2
+    dec_input resb 32 
 
 section .text
     global _start
-
+    
 _start:
-    print data_size_prompt, data_size_prompt_len
+    ;display input prompt
+    mov eax, ip_prompt
+    call sprint
 
-    read data_size, 3
+    ;get user input for decimal
+    mov eax, SYS_READ
+    mov ebx, STDIN
+    mov ecx, dec_input
+    mov edx, 32
+    int 80h
 
-    print data_size, 2
+    ;convert ascii input to integer
+    mov eax, dec_input
+    call atoi
+
+    ;set counter to 0
+    mov ecx, 0
+
+
+;convert decimal to binary
+binary_conversion:
+    ;divide input by 2
+    xor edx, edx
+    mov ebx, 2
+    div ebx
+
+    ;push remainder into the stack
+    push edx
+
+    ;increment counter
+    inc ecx
+
+    ;check if decimal is equal to 0
+    cmp eax, 0
+
+    ;continue to loop until dec is equal to 0
+    jne binary_conversion
+
+
+;pop all the remainders to display the binary result
+pop_edx:
+    pop eax
+    call iprint
+    dec ecx
+    cmp ecx, 0
+    jne pop_edx
+    
 
     ;exit program
-    mov eax, SYS_EXIT
-    int 80h
+    call quit
